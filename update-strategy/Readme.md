5ce1d93fde8e51c76d02e94182ea156e1a064ef2
Mostafa <mostafa.negim@gmail.com>
2021-10-22 14:29:00 +0330

    Update: readme and deploy

--- update-strategy/Readme.md
+++ update-strategy/Readme.md
@@ -1,7 +1,12 @@
-1) Get the default update strategy:
+1. Get the default update strategy:
 
 ```kubectl describe deployment simple-nginx | grep StrategyType```
 
-2) Get the ReplicaSet:
+1. Get the ReplicaSet:
 
 ```kubectl describe deployment simple-nginx | grep ReplicaSet```
+
+1. Edit Deployment
+
+1. As you see, a new replica set simple-nginx-694f94f77d is created in the Deployment simple-nginx. Each time the new ReplicaSet scales one Pod up successfully, the old ReplicaSet will scale one Pod down. The scaling process
+finishes at the moment that the new ReplicaSet meets the original desired Pod number (as said, 5 Pods), and the old ReplicaSet has zero Pods.
--- update-strategy/deploy.sh
+++ update-strategy/deploy.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+kubectl apply -f deploy-simple-nginx.v1.0.yaml
+kubectl apply -f service-simple-nginx-v1.0.yaml
+
+kubectl describe deployment simple-nginx
+
+
+export SERVICE_URL=$(minikube service nginx-service --url)
+
+curl $SERVICE_URL
+kubectl edit deployment simple-nginx --record
+
+# ADD
+# command:
+# - sh
+# - -c
+# - echo "Happy Programming with Kubernetes!" >
+# /usr/share/nginx/html/index.html && service nginx stop && nginx
+# -g "daemon off;"
+
+curl $SERVICE_URL
+kubectl rollout status deployment simple-nginx
+kubectl describe deployment simple-nginx
+
+kubectl get rs
+
+k describe rs simple-nginx-6665dd75f | grep Selector
+
+# Rollback
+kubectl rollout history deployment simple-nginx
