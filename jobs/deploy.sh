44c36c81243d2fa2c879546ad1c9b67dd7fe8fb2
Mostafa <mostafa.negim@gmail.com>
2021-10-24 16:19:55 +0330

    Add cronjob

--- jobs/deploy.sh
+++ jobs/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 kubectl create -f job-dpkg.yaml
+
+k delete jobs.batch package-check
+
+# Repeatable Job
+k apply -f job-dpkg-repeat.yaml
+k delete jobs.batch package-check
+
+# Parallel Job
+k apply -f job-dpkg-parallel.yaml
+k delete jobs.batch package-check
+
+# Cron jobs
+k apply -f cron-job.yaml
+k delete cronjobs.batch package-check
