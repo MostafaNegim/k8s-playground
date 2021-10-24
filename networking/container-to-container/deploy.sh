05e8b207204f2ecd73dfffe01df916d51a05d6da
Mostafa <mostafa.negim@gmail.com>
2021-10-24 13:02:24 +0330

    Update container-to-container

--- networking/container-to-container/deploy.sh
+++ networking/container-to-container/deploy.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+kubectl create -f two-container-pod.yaml
+# check the status of the newly-created Pod
+kubectl get pod two-container
+kubectl describe pod two-container
+
+kubectl logs two-container centos | grep "title"
+
+# Go to the pod
+# minikube ssh && docker ps | grep "two-container"
+# docker inspect 2e5dd7861ed3 | jq '.[]| {NetworkMode: .HostConfig.NetworkMode, NetworkSettings: .NetworkSettings}'
